599556bab66d04a265f226d0082865c91c418971
sooryajith77 <soorya8590agmail.com>
2026-04-27 16:44:53 +0530

    config api

--- ff-deploy/src/config/apiConfig.js
+++ ff-deploy/src/config/apiConfig.js
@@ -1,1 +1,2 @@
-export const API_BASE_URL = process.env.NEXT_PUBLIC_API_URL || "https://crm-kq7h.onrender.com";
+export const API_BASE_URL =
+  process.env.NEXT_PUBLIC_API_URL || "https://crm-kq7h.onrender.com/api";
